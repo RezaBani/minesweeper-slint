cc80f8c26c52dae1305a37f85763ae7dab1b38ca
RezaBanivakil <rezabaniv@gmail.com>
2025-12-28 18:51:05 +0330

    Naming improved
    ui files directory renamed to ui
    build script now builds all the .slint files in ui folder (this will be removed it's just here for reference)

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, path::PathBuf};
 
 use slint_build::CompileError;
 
@@ -6,11 +6,26 @@ fn main() -> Result<(), CompileError> {
     unsafe {
         env::set_var("RUST_BACKTRACE", "1");
     }
-    let input_files = ["./src/mine_sweeper_ui/main_window.slint"];
+
+    let input_path = PathBuf::from("./src/ui");
+
+    let input_files: Vec<_> = input_path
+        .read_dir()
+        .unwrap()
+        .into_iter()
+        .filter_map(|fi| {
+            let path = fi.unwrap().path();
+            if path.is_file() && path.to_string_lossy().ends_with(".slint") {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect();
 
     for input_file in input_files {
-        slint_build::compile(input_file)?;
-        println!("cargo::rerun-if-changed={input_file}");
+        slint_build::compile(&input_file)?;
+        println!("cargo::rerun-if-changed={}", input_file.to_string_lossy());
     }
     println!("cargo::rerun-if-changed=Cargo.toml");
     Ok(())
